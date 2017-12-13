99a8896324ebe988f7f67419c217c7fb23c14647
Alison Stuart <alisonstuart@Alisons-MacBook-Pro.local>
2017-12-13 14:17:17 -0500

    pared it down a bit

--- src/server-express.js
+++ src/server-express.js
@@ -8,6 +8,10 @@ var ejs = require('ejs');
 const server = express();
 const PORT = process.env.PORT || 5000;
 
+// server.use(server.router);
+server.set('views', __dirname + '/views');
+server.use(express.static(path.join(__dirname, 'public')));
+
 server.use(bodyParser.json());
 server.set('view engine', 'ejs');
 
@@ -22,6 +26,10 @@ server.get('/chat', (req, res) => {
     });
   }
   return 'error in server.get() in server-express.js in src folder!';
+});
+
+server.get('/home', (req, res) => {
+   return 'Welcome to the landing page!'            
 });
 
 const options = {
